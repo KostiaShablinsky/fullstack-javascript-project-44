c0fec14bcef7e028b2115cbb5f0333fe4e59d1c5
Konstantin <constantindemon@yandex.ru>
2024-08-06 19:13:21 +0300

    .eslintrc.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function firstName(nameUser) {
+export default function firstName() {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
