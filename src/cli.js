ada366af49e2b8017699f3a8c1a55a813e61882e
Konstantin <constantindemon@yandex.ru>
2024-08-06 18:30:58 +0300

    bin/brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
-import readlineSync from 'readline-sync'
-export default function firstName(userName){
-const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name + '!') 
+import readlineSync from 'readline-sync';
+
+export default function firstName(nameUser) {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 }
